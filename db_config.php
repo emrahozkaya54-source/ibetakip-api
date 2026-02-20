d5428e06a8373700c30e855b14f5d152647fa5af
emrahozkaya54-source <emrahozkaya54@gmail.com>
2026-02-20 22:53:14 +0300

    db_config.php

--- db_config.php
+++ db_config.php
@@ -2,5 +2,5 @@
 define('DB_DATABASE', "b31_41196220_ibetakip");
 define('DB_USER', "b31_41196220");
 define('DB_PASSWORD', "Emrh1944.");
-define('DB_SERVER', "sql100.byethost31.com"); 
+define('DB_SERVER', "https://sql100.byethost31.com"); 
 ?>
